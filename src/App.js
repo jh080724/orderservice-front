4343ba6227da92e76fea65c2fa824c471d947634
jh080724 <tsj010813@gmail.com>
2024-11-06 12:34:17 +0900

    상품리스트 조회, 장바구니에 상품추가

--- src/App.js
+++ src/App.js
@@ -7,21 +7,26 @@ import { Route, Routes } from 'react-router-dom';
 import MemberCreate from './components/MemberCreate';
 import LoginPage from './components/LoginPage';
 import { AuthContextProvider } from './context/UserContext';
+import ProductList from './components/ProductList';
+import { CartContextProvider } from './context/CartContext';
 
 function App() {
   return (
     <AuthContextProvider>
-      <div className='App'>
-        <Header />
-        <div className='content-wrapper'>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/member/create' element={<MemberCreate />} />
-            <Route path='/login' element={<LoginPage />} />
-          </Routes>
+      <CartContextProvider>
+        <div className='App'>
+          <Header />
+          <div className='content-wrapper'>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/member/create' element={<MemberCreate />} />
+              <Route path='/login' element={<LoginPage />} />
+              <Route path='/product/list' element={<ProductList />} />
+            </Routes>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </CartContextProvider>
     </AuthContextProvider>
   );
 }
---
+++ src/components/ProductList.js
@@ -0,0 +1,231 @@
+import {
+  Button,
+  Card,
+  CardContent,
+  Checkbox,
+  Container,
+  Grid,
+  MenuItem,
+  Select,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  TextField,
+  Typography,
+} from '@mui/material';
+import React, { useContext, useEffect, useState } from 'react';
+import AuthContext from '../context/UserContext';
+import CartContext from '../context/CartContext';
+
+const ProductList = ({ pageTitle }) => {
+  const [searchType, setSearchType] = useState('optional');
+  const [searchValue, setSearchValue] = useState('');
+  const [productList, setProductList] = useState([]);
+  const [selected, setSelected] = useState({});
+
+  const { userRole } = useContext(AuthContext);
+  const { addCart } = useContext(CartContext);
+  const isAdmin = userRole === 'ADMIN';
+
+  useEffect(() => {
+    loadProduct();
+  }, []);
+
+  // 상품 목록을 백앤드에 요청하는 함수.
+  const loadProduct = async (number, size) => {
+    const res = await fetch(
+      `${process.env.REACT_APP_API_BASE_URL}/product/list`,
+    );
+
+    // console.log(res.json());
+
+    const data = await res.json();
+    // console.log(data);
+
+    setProductList(data.result);
+  };
+
+  // 장바구니 클릭 이벤트 핸들러
+  const handleAddToCart = () => {
+    // 특정 객체에서 KEy값만 뽑아서 문자열 배열로 리턴해주는 메서드
+    const selectedProduct = Object.keys(selected); // 키값만 뽑아서 배열에 담기.
+
+    // Key 값만 뽑아서 Selected에 들어있는 상품들 중에 false인 거 빼고 true인것만 담겠다.
+    const filtered = selectedProduct.filter((key) => selected[key]);
+    const finalSelected = filtered.map((key) => {
+      const product = productList.find((p) => p.id === parseInt(key));
+      return {
+        id: product.id,
+        name: product.name,
+        quantity: product.quantity,
+      };
+    });
+
+    console.log('최종선택: ', finalSelected);
+
+    if (finalSelected.length < 1) {
+      alert('장바구니에 추가할 상품을 선택해 주세요!.');
+      return;
+    }
+
+    for (let p of finalSelected) {
+      if (!p.quantity) {
+        alert('수량이 0개인 상품은 담을 수 없습니다.');
+        return;
+      }
+    }
+
+    if (confirm('상품을 장바구니에 추가하시겠습니까?')) {
+      // 카드로 상품을 보내주자.
+      finalSelected.forEach((product) => addCart(product));
+      alert('선택한 상품이 장바구니에 추가되었습니다.');
+    }
+  };
+
+  // 체크박스 클릭 이벤트 핸들러
+  const handleCheckboxChange = (productId, checked) => {
+    // 사용자가 특정상품을 선택했는지에 대한 상태를 관리{상품아이디, 체크여부}
+    setSelected((prevSelected) => ({
+      ...prevSelected, // 기존에 담겨있는 것은 그대로 두고...
+      [productId]: checked,
+    }));
+  };
+  console.log(selected);
+
+  return (
+    <Container>
+      <Grid
+        container
+        justifyContent='space-between'
+        spacing={2}
+        className='mt-5'
+      >
+        <Grid item>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              loadProduct();
+            }}
+          >
+            <Grid container spacing={2}>
+              <Grid item>
+                <Select
+                  value={searchType}
+                  onChange={(e) => setSearchType(e.target.value)}
+                  displayEmpty
+                >
+                  <MenuItem value='optional'>선택</MenuItem>
+                  <MenuItem value='name'>상품명</MenuItem>
+                  <MenuItem value='category'>카테고리</MenuItem>
+                </Select>
+              </Grid>
+              <Grid item>
+                <TextField
+                  value={searchValue}
+                  onChange={(e) => setSearchValue(e.target.value)}
+                  label='Search'
+                />
+              </Grid>
+              <Grid item>
+                <Button type='submit'>검색</Button>
+              </Grid>
+            </Grid>
+          </form>
+        </Grid>
+
+        {!isAdmin && (
+          <Grid item>
+            <Button onClick={handleAddToCart} color='secondary'>
+              장바구니
+            </Button>
+          </Grid>
+        )}
+
+        {isAdmin && (
+          <Grid item>
+            <Button href='/product/create' color='success'>
+              상품등록
+            </Button>
+          </Grid>
+        )}
+      </Grid>
+
+      <Card>
+        <CardContent>
+          <Typography variant='h6' align='center'>
+            {pageTitle}
+          </Typography>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>제품사진</TableCell>
+                <TableCell>제품명</TableCell>
+                <TableCell>가격</TableCell>
+                <TableCell>재고수량</TableCell>
+                {!isAdmin && <TableCell>주문수량</TableCell>}
+                {!isAdmin && <TableCell>주문선택</TableCell>}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {productList.map((product) => (
+                <TableRow key={product.id}>
+                  <TableCell>
+                    <img
+                      src={product.imagePath}
+                      alt={product.name}
+                      style={{ height: '100px', width: 'auto' }}
+                    />
+                  </TableCell>
+                  <TableCell>{product.name}</TableCell>
+                  <TableCell>{product.price}</TableCell>
+                  <TableCell>{product.stockQuantity}</TableCell>
+                  {!isAdmin && (
+                    <TableCell>
+                      <TextField
+                        type='number'
+                        value={product.quantity || 0}
+                        onChange={(e) =>
+                          // 사용자가 주문 수량을 올릴때마다
+                          // 상품객체에 quantity라는 프로퍼티를 추가해서
+                          // 추후에 장바구니에 담을 때 꺼내서 활용할 예정
+                          setProductList((prevList) =>
+                            prevList.map((p) =>
+                              p.id === product.id
+                                ? { ...p, quantity: parseInt(e.target.value) }
+                                : p,
+                            ),
+                          )
+                        }
+                        style={{ width: '70px' }}
+                        InputProps={{
+                          inputProps: {
+                            min: 0,
+                            max: product.stockQuantity,
+                          },
+                        }}
+                      />
+                    </TableCell>
+                  )}
+                  {!isAdmin && (
+                    <TableCell>
+                      <Checkbox
+                        checked={!!selected[product.id]}
+                        onChange={(e) =>
+                          handleCheckboxChange(product.id, e.target.checked)
+                        }
+                      />
+                    </TableCell>
+                  )}
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </CardContent>
+      </Card>
+    </Container>
+  );
+};
+
+export default ProductList;
